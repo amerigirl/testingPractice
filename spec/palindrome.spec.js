265a3acffe783e40509083e65becb014105c5c19
Be-Asia <93811176+amerigirl@users.noreply.github.com>
2023-02-27 15:33:51 +0000

    Initial commit

---
+++ spec/palindrome.spec.js
@@ -0,0 +1,12 @@
+const isPalindrome = require('../Palindrome.js');
+
+describe("isPalindrome", function(){
+
+  it("should return true for a single letter", function(){
+    expect(isPalindrome("a")).toBeTrue();
+  });
+
+  it("should return true for a single letter repeated", )
+
+  
+})
